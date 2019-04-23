140b9f17019706a99673ef446c1403d36d7b92a1
unknown <grantludwig97@gmail.com>
2019-04-23 15:08:23 -0700

    colChecking complete

--- hw2.py
+++ hw2.py
@@ -4,15 +4,9 @@ import sys
 def buildBoard(size):
     board = []
     for i in range(0,size):
-        board.append([False, False, False])
+        board.append([False, False, False, False])
     return board
 
-def buildKnown(size):
-    board = []
-    for i in range(0,size):
-        board.append([True, True, True])
-    return board
-   
 def printBoard(board):
     for row in board:
         rowStr = '|'
@@ -23,9 +17,35 @@ def printBoard(board):
         print(rowStr)
         print(underStr)
 
+def colCheck(board):
+    attack = False
+    position = []
+    for row in board:
+        for colNum in range(len(row)):
+            if row[colNum]:
+                if colNum in position:
+                    attack = True
+                position.append(colNum)
+    return attack
+
+def diagcheck(board):
+    attack = False
+    return attack
+
+def goodBoard(board):
+    attack = colCheck(board)
+    if not attack:
+        attack = diagcheck(board)
+    
+    return attack
 
 #driver code
 boardQueenAmount = sys.argv[1]
 board = buildBoard(int(boardQueenAmount))
 print(board, '\n')
 printBoard(board)
+print('\n')
+board = [[True, False, False, False], [False, False, False, False], [False, False, False, False], [False, False, False, True]]
+printBoard(board)
+print('\n')
+print(goodBoard(board))
